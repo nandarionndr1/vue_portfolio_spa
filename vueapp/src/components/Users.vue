d1fb86d773a586cbb9cab48044782bde5953a53c
nandarionndr1 <leebet16@gmail.com>
2020-02-19 00:21:07 +0800

    initial work around

---
+++ vueapp/src/components/Users.vue
@@ -0,0 +1,13 @@
+<template>
+    <h3>Users: succ dic xd</h3>
+</template>
+<script>
+    export default{
+        name: 'Users',
+    }
+</script>
+<style>
+    h3{
+        margin-bottom: 5%;
+    }
+</style>
